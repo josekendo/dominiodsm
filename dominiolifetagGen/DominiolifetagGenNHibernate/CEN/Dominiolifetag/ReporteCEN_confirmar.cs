146e6029dcc43b796bbd5e252ba175dd43d38755
Unknown <mariamsm94@hotmail.com>
2017-11-07 10:46:28 +0100

    Cen de María (Confirmación lo pasa a true y lo cambia en el CAD)

--- dominiolifetagGen/DominiolifetagGenNHibernate/CEN/Dominiolifetag/ReporteCEN_confirmar.cs
+++ dominiolifetagGen/DominiolifetagGenNHibernate/CEN/Dominiolifetag/ReporteCEN_confirmar.cs
@@ -21,9 +21,15 @@ public partial class ReporteCEN
 {
 public bool Confirmar (int p_oid)
 {
+
         /*PROTECTED REGION ID(DominiolifetagGenNHibernate.CEN.Dominiolifetag_Reporte_confirmar) ENABLED START*/
 
         // Write here your custom code...
+        ReporteEN report = _IReporteCAD.ReadOIDDefault(p_oid);
+
+        report.Confirmacion = true;
+
+        _IReporteCAD.Modify(report);
 
         throw new NotImplementedException ("Method Confirmar() not yet implemented.");
 
