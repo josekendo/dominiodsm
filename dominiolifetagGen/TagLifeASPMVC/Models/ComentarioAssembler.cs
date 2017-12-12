98d3f82451fad82b158d187b77049e00d0cdc7af
dirtdiver353 <dirtdiver353@gmail.com>
2017-12-12 10:26:31 +0100

    subir comentario asembler
    version1

---
+++ dominiolifetagGen/TagLifeASPMVC/Models/ComentarioAssembler.cs
@@ -0,0 +1,29 @@
+﻿using DominiolifetagGenNHibernate.EN.Dominiolifetag;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//Los assembler los creamos para pasar los datos en a datos que entienda el motor web 
+//aqui tenemos declarado un unico dato y luego una lista 
+namespace TagLifeASPMVC.Models
+{
+    public class ComentarioAssembler
+    {
+        public Comentario ComentarioENToModelUI(ComentarioEN en)
+        {
+          Comentario cat = new Comentario ();
+          cat.Contenido = en.Contenido;
+          return cat;
+        }
+        public IList<Comentario> ConvertListENToModel(IList<ComentarioEN> ens)
+        {
+            IList<Comentario> arts = new List<Comentario>();
+            foreach (ComentarioEN en in ens)
+            {
+                arts.Add(ComentarioENToModelUI(en));
+            }
+            return arts;
+        }
+    }
+}
